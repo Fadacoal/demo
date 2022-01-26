ba7d46cfe061acf28e6cdab3c74cda670a035916
bx42bn <Arjean.Dequiroz@ing.com>
2022-01-26 14:24:48 +0800

    second trial for IO

--- src/main/java/JavaTutorial.java
+++ src/main/java/JavaTutorial.java
@@ -1,29 +1,36 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class JavaTutorial {
 
     public static void main(String[] args) throws IOException {
-        FileInputStream in = null;
-        FileOutputStream out = null;
+        createDirectory();
 
         try {
-            in = new FileInputStream("input.txt");
-            out = new FileOutputStream("output.txt");
+            byte bWrite [] = {11,21,3,40,5};
+            OutputStream os = new FileOutputStream("test.txt");
+            for(int x = 0; x < bWrite.length ; x++) {
+                os.write( bWrite[x] );   // writes the bytes
+            }
+            os.close();
 
-            int c;
-            while ((c = in.read()) != -1) {
-                out.write(c);
+            InputStream is = new FileInputStream("test.txt");
+            int size = is.available();
+
+            for(int i = 0; i < size; i++) {
+                System.out.print((byte)is.read() + "  "); // char conversion was not an alphanumeric, it output the byte
             }
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-            if (out != null) {
-                out.close();
-            }
+            is.close();
+        } catch (IOException e) {
+            System.out.print("Exception");
         }
+    }
+
+    public static void createDirectory(){
+        String dirname = "/Users/bx42bn/Downloads/demo/src/main/java/newDirectory";
+        File d = new File(dirname);
+
+        // Create directory now.
+        d.mkdirs();
     }
 }
 
