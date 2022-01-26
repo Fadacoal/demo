09da679c2e56dfcab270d4779b10a56814cf94aa
bx42bn <Arjean.Dequiroz@ing.com>
2022-01-27 05:35:23 +0800

    Exception with try with resources

--- src/main/java/JavaTutorial.java
+++ src/main/java/JavaTutorial.java
@@ -3,22 +3,13 @@ import java.io.*;
 public class JavaTutorial {
 
     public static void main(String[] args) throws IOException {
-        FileReader fr = null;
-        try {
-            File file = new File("trial.txt");
-            fr = new FileReader(file);
+        try (FileReader fr = new FileReader("E://file.txt")){
             char [] a = new char[50];
             fr.read(a);   // reads the content to the array
             for(char c : a)
                 System.out.print(c);   // prints the characters one by one
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            try {
-                fr.close();
-            } catch (NullPointerException | IOException ex ) {
-                ex.printStackTrace();
-            }
         }
     }
 }
