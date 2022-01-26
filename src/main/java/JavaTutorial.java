91f1a6cebbabc99d73d508647e55d9084bb9a33e
bx42bn <Arjean.Dequiroz@ing.com>
2022-01-26 14:28:43 +0800

    3rd trial for IO

--- src/main/java/JavaTutorial.java
+++ src/main/java/JavaTutorial.java
@@ -6,31 +6,55 @@ public class JavaTutorial {
         createDirectory();
 
         try {
-            byte bWrite [] = {11,21,3,40,5};
+            byte bWrite[] = {11, 21, 3, 40, 5};
             OutputStream os = new FileOutputStream("test.txt");
-            for(int x = 0; x < bWrite.length ; x++) {
-                os.write( bWrite[x] );   // writes the bytes
+            for (int x = 0; x < bWrite.length; x++) {
+                os.write(bWrite[x]);   // writes the bytes
             }
             os.close();
 
             InputStream is = new FileInputStream("test.txt");
             int size = is.available();
 
-            for(int i = 0; i < size; i++) {
-                System.out.print((byte)is.read() + "  "); // char conversion was not an alphanumeric, it output the byte
+            for (int i = 0; i < size; i++) {
+                System.out.print((byte) is.read() + "  "); // char conversion was not an alphanumeric, it output the byte
             }
             is.close();
         } catch (IOException e) {
             System.out.print("Exception");
         }
+
+        listDirectory();
     }
 
-    public static void createDirectory(){
+    public static void createDirectory() {
         String dirname = "/Users/bx42bn/Downloads/demo/src/main/java/newDirectory";
         File d = new File(dirname);
 
         // Create directory now.
         d.mkdirs();
+    }
+
+    public static void listDirectory() {
+        File file = null;
+        String[] paths;
+
+        try {
+            // create new file object
+            file = new File("/tmp");
+
+            // array of files and directory
+            paths = file.list();
+
+            // for each name in the path array
+            for (String path : paths) {
+                // prints filename and directory name
+                System.out.println(path);
+            }
+        } catch (Exception e) {
+            // if any error occurs
+            e.printStackTrace();
+        }
     }
 }
 
